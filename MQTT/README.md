0a1edac95c59d27ceae4877f557b334e7cf6d9c1
Dao Hua <daohuaptit@gmail.com>
2023-09-09 17:19:37 +0700

    Add simple mqtt subscribe program

---
+++ MQTT/README.md
@@ -0,0 +1,1 @@
+This is a simple submqtt program using mosquitto library.
---
+++ MQTT/SimpleMQTTpub/mqtt_pub.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <mosquitto.h>
+int main()
+{
+    mosquitto_lib_init();
+    printf("Hello\n");
+    return 0;
+}
