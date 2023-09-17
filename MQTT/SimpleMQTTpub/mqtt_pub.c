1146a21cfe11e1e5438538453f0e97dda8a1c698
Dannyy <93213627+daohuaptit@users.noreply.github.com>
2023-09-17 15:13:06 +0700

    Update mqtt_pub.c
    Delete some comments

--- MQTT/SimpleMQTTpub/mqtt_pub.c
+++ MQTT/SimpleMQTTpub/mqtt_pub.c
@@ -4,7 +4,5 @@ int main()
 {
 	/*Initilize the mosquitto lib. This should be done before anything else*/
     mosquitto_lib_init();
-    printf("Hello\n");
-	/*This is where you are implementing your code*/
     return 0;
 }
