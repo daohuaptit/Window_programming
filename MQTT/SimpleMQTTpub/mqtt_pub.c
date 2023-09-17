eb93576a054ab802fe22449f2e2e53cdffa0a7f0
Dao Hua <daohuaptit@gmail.com>
2023-09-17 14:24:18 +0700

    Add comment to mqtt_sub

--- MQTT/SimpleMQTTpub/mqtt_pub.c
+++ MQTT/SimpleMQTTpub/mqtt_pub.c
@@ -5,5 +5,6 @@ int main()
 	/*Initilize the mosquitto lib. This should be done before anything else*/
     mosquitto_lib_init();
     printf("Hello\n");
+	/*This is where you are implementing your code*/
     return 0;
 }
